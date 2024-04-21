2eb920c03d23de78fb4dbffe7e748dd9ab8709f9
bastide.antonin <antonin.bastide@fortil.group>
2024-04-21 17:18:07 +0200

    Suppression d'import non utilisé

--- src/main/java/fr/chat/application/entities/Salon.java
+++ src/main/java/fr/chat/application/entities/Salon.java
@@ -1,12 +1,8 @@
 package fr.chat.application.entities;
 
-import java.util.ArrayList;
-import java.util.List;
-
 public class Salon {
 
     private String name;
-
 
     public Salon(String name) {
         super();
