428d4f9ad1d5141607a3d25060749cf29fa4399b
bastide.antonin <antonin.bastide@fortil.group>
2024-04-27 22:29:40 +0200

    MaJ test pour get les data d'un fichier csv

--- src/main/java/fr/chat/application/Main.java
+++ src/main/java/fr/chat/application/Main.java
@@ -1,12 +1,21 @@
 package fr.chat.application;
 
 
+import fr.chat.application.entities.Utilisateur;
+import fr.chat.application.repositories.SalonRepositoryFile;
 import fr.chat.application.repositories.SalonRepositoryJson;
 import fr.chat.application.services.SalonService;
 
+import javax.sound.sampled.Line;
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+
 public class Main  {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
 
         SalonService salonService = new SalonService(new SalonRepositoryJson("C:\\Users\\antonin.bastide\\Documents\\ProjetPerso\\chat-application\\src\\main\\java\\fr\\chat\\application\\BDD.json"));
         salonService.getAllSalons();
@@ -24,6 +33,11 @@ public class Main  {
             System.out.println(salon);
         }
         */
+
+        String utilisateursCSV = "C:\\Users\\antonin.bastide\\Documents\\ProjetPerso\\chat-application\\src\\main\\java\\fr\\chat\\application\\utilisateurs.csv";
+        var repo = new SalonRepositoryFile(utilisateursCSV);
+            var utilisateurs = repo.getAllUtilisateurs();
+           System.out.println(utilisateurs);
 
 
     }
