72de1249d663d45f62fe50458b8c6a1d4a870d3c
bastide.antonin <antonin.bastide@fortil.group>
2024-04-28 23:29:21 +0200

    MaJ

--- src/main/java/fr/chat/application/Main.java
+++ src/main/java/fr/chat/application/Main.java
@@ -3,18 +3,17 @@ package fr.chat.application;
 import fr.chat.application.repositories.SalonRepositoryFile;
 import fr.chat.application.repositories.SalonRepositoryJson;
 import fr.chat.application.services.SalonService;
-import java.io.IOException;
 
 
 public class Main  {
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
 
         String utilisateursCheminCSV = ".\\src\\main\\java\\fr\\chat\\application\\utilisateurs.csv";
         String salonsUtilisateursCheminCSV = ".\\src\\main\\java\\fr\\chat\\application\\salonsUtilisateurs.csv";
 
         SalonService salonService = new SalonService(new SalonRepositoryJson(".\\src\\main\\java\\fr\\chat\\application\\BDD.json"));
-        SalonService salonServiceFile = new SalonService(new SalonRepositoryFile(utilisateursCheminCSV, salonsUtilisateursCheminCSV));
+        SalonService salonServiceFile = new SalonService(new SalonRepositoryFile(utilisateursCheminCSV, salonsUtilisateursCheminCSV, ""));
         //System.out.println(salonService.getUtilisateurSalonsById(2));
         System.out.println(salonServiceFile.getAllSalonsUtilisateurs());
         System.out.println(salonServiceFile.getAllUtilisateurs());
